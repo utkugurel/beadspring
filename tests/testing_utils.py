8d29a6ba92d533e184a075d39f1aa4f5ceb1f588
Utku Gürel <utkugurel@gmail.com>
2024-03-07 14:38:56 +0100

    added utilities module for tests

---
+++ tests/testing_utils.py
@@ -0,0 +1,23 @@
+"""Utility functions for testing"""
+
+import MDAnalysis as mda
+import numpy as np
+import freud
+from pyprojroot.here import here
+
+def setup_universe():
+    """Set up mdanalysis universe before testing"""
+    topology_file = here('tests/data/topo.data')
+    trajectory_file = here('tests/data/traj.dat')
+    u = mda.Universe(topology_file,
+                     trajectory_file,
+                     format='LAMMPSDUMP',
+                     dt=0.005)
+    return u
+
+
+def setup_freud():
+    universe = setup_universe()
+    box = freud.Box.cube(universe.dimensions[0])
+    return box 
+
