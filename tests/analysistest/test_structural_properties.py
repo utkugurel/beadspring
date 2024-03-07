b87996eb1d90526e33dec2d82799fa78d184ece9
Utku Gürel <utkugurel@gmail.com>
2024-03-07 14:39:38 +0100

    created tests for the structural properties

---
+++ tests/analysistest/test_structural_properties.py
@@ -0,0 +1,14 @@
+"""Tests for the beadspring.analysis.structural_properties module."""
+import pytest
+import freud
+import MDAnalysis as mda
+import numpy as np
+import numpy.testing as npt
+from pyprojroot.here import here
+from tests.testing_utils import setup_universe, setup_freud
+
+
+#universe = setup_universe()
+#box = setup_freud()
+
+#TODO: Add tests for the structural properties module
