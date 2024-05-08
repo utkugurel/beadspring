d94ef5aa1341f351f19b155ab3e44a2cfa2944f9
utku <19888110+utkugurel@users.noreply.github.com>
2024-05-08 14:18:01 +0200

    package version updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Bead Spring Analytics',  # Replace with your package's name
-    version='1.0.0',  # Package version
+    version='0.1.0',  # Package version
     author='Utku Gürel',  # Your name or your organization's name
     author_email='utkugurel@gmail.com',  # Your email or your organization's contact email
     description='Analysis of beadspring polymers',  # A short description of the package
@@ -20,16 +20,3 @@ setup(
     ],
     python_requires='>=3.11.6',  # Minimum version requirement of the package
 )
-
-# Welcome message
-
-ascii_image = r"""  ____                    _    _____               _                                         _         _    _            
- |  _ \                  | |  / ____|             (_)                   /\                  | |       | |  (_)           
- | |_) |  ___   __ _   __| | | (___   _ __   _ __  _  _ __    __ _     /  \    _ __    __ _ | | _   _ | |_  _   ___  ___ 
- |  _ <  / _ \ / _` | / _` |  \___ \ | '_ \ | '__|| || '_ \  / _` |   / /\ \  | '_ \  / _` || || | | || __|| | / __|/ __|
- | |_) ||  __/| (_| || (_| |  ____) || |_) || |   | || | | || (_| |  / ____ \ | | | || (_| || || |_| || |_ | || (__ \__ \
- |____/  \___| \__,_| \__,_| |_____/ | .__/ |_|   |_||_| |_| \__, | /_/    \_\|_| |_| \__,_||_| \__, | \__||_| \___||___/
-                                     | |                      __/ |                              __/ |                   
-                                     |_|                     |___/                              |___/                    """
-
-print(ascii_image)
