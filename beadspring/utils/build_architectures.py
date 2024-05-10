6459028ba97d151f29c47c39b4da8bd08e9e48d2
utku <19888110+utkugurel@users.noreply.github.com>
2024-05-10 15:08:21 +0200

    added function to create a polydisperse star

--- beadspring/utils/build_architectures.py
+++ beadspring/utils/build_architectures.py
@@ -538,3 +538,263 @@ def create_2d_star(num_arms, arm_length, file_name=None):
         f.write('\n')
         f.write('Bonds\n\n')
         np.savetxt(f, bond, fmt='%4.0f %6.0f %6.0f %6.0f')
+
+def create_polydisperse_star(f1, M1, f2, M2, file_name=None):
+    '''
+    Create a polydisperse star polymer with a given number of arms and arm length.
+    The core is a sphere with radius R = 0.5.
+    The arms are composed of monomers with a bond length of 0.97.
+    The charge is zero for all atoms.
+    The function creates a star polymer with f1 arms of length M1 and f2 arms of length M2.
+    
+    Parameters
+    ----------
+    f1 : int
+        Number of arms in the star polymer.
+    M1 : int
+        Number of monomers in each arm.
+    f2 : int
+        Number of arms in the star polymer.
+    M2 : int
+        Number of monomers in each arm.
+    save_file : str
+        File name to save the data file. Default None will save as 'star.data'.
+
+    Returns
+    -------
+    None
+
+    '''
+
+
+    filename = 'start/star.data' # save direction
+
+    f1 = int(sys.argv[1])
+    M1 = int(sys.argv[2])
+
+    f2 = int(sys.argv[3])
+    M2 = int(sys.argv[4])
+
+
+    num_arms = f1 + f2
+    num_atoms = f1*M1 + f2*M2 + 1
+    R = 0.5
+    D = 2 * R
+
+    coor_core = np.zeros(3)
+
+    # Polymer parameters
+
+    blen1 = 0.97
+    dmin = 0.8
+    rho_polymer = 1.
+    monomer_mass = 1
+    core_mass = D**3
+    N_polymer = f1*M1 + f2*M2
+
+    xlo = -10000 # box boundaries
+    xhi = 100000
+    ylo = -10000
+    yhi = 100000
+    zlo = -10000
+    zhi = 100000
+
+    coor_polymer1 = np.zeros((f1, M1, 3))
+
+    for j in range(f1):  # chain loop
+        coor = np.zeros((M1,3)) 
+        for i in range(M1):  # monomer loop for each chain
+            if i==0:
+                ph = np.pi * np.random.rand()
+                th = 2 * np.pi * np.random.rand()
+                xij = (R+0.5)*np.sin(ph)*np.cos(th) # initial position for each chain
+                yij = (R+0.5)*np.sin(ph)*np.sin(th)
+                zij = (R+0.5)*np.cos(ph)
+            else:
+                restriction = True  # coordinate of the first B atom for the first A atom
+                while restriction:
+                    dx = 2.0 * np.random.rand() - 1.0
+                    dy = 2.0 * np.random.rand() - 1.0
+                    dz = 2.0 * np.random.rand() - 1.0
+                    rsq = dx**2 + dy**2 + dz**2
+                    r = np.sqrt(rsq)
+                    dx = dx / r
+                    dy = dy / r
+                    dz = dz / r
+                    xij = coor[i-1][0] + dx * blen1
+                    yij = coor[i-1][1] + dy * blen1
+                    zij = coor[i-1][2] + dz * blen1
+                    restriction = False
+                    # all monomers outside of NP
+                    if (np.sqrt(xij**2 + yij**2 + zij**2) < R+0.5):
+                        restriction = True
+                    if i >= 2:
+                        distx = xij - coor[i-2][0]
+                        disty = yij - coor[i-2][1]
+                        distz = zij - coor[i-2][2]
+                        if (np.sqrt(distx*distx+ disty*disty + distz*distz) <= dmin): # the minimum distance between atom 1 and atom 3
+                            restriction = True
+                        elif ((xij<xlo) or (xij>xhi) or (yij<ylo) or (yij>yhi) or (zij<zlo) or (zij>zhi)):
+                            restriction = True
+                        #elif ((xij-5)**2 + (yij-5)**2 + (zij-5)**2) < 25 
+                        #   restriction = True
+            coor[i][0] = xij
+            coor[i][1] = yij
+            coor[i][2] = zij
+        coor_polymer1[ j, : ] = coor[:,:]
+
+    coor_polymer1 = coor_polymer1.reshape(f1*M1, 3)
+    coor_core = coor_core.reshape((1,3))
+    coords = np.concatenate((coor_core, coor_polymer1))
+
+
+    coor_polymer2 = np.zeros((f2, M2, 3))
+
+    for j in range(f2):  # chain loop
+        coor = np.zeros((M2,3)) 
+        for i in range(M2):  # monomer loop for each chain
+            if i==0:
+                ph = np.pi * np.random.rand()
+                th = 2 * np.pi * np.random.rand()
+                xij = (R+0.5)*np.sin(ph)*np.cos(th) # initial position for each chain
+                yij = (R+0.5)*np.sin(ph)*np.sin(th)
+                zij = (R+0.5)*np.cos(ph)
+            else:
+                restriction = True  # coordinate of the first B atom for the first A atom
+                while restriction:
+                    dx = 2.0 * np.random.rand() - 1.0
+                    dy = 2.0 * np.random.rand() - 1.0
+                    dz = 2.0 * np.random.rand() - 1.0
+                    rsq = dx**2 + dy**2 + dz**2
+                    r = np.sqrt(rsq)
+                    dx = dx / r
+                    dy = dy / r
+                    dz = dz / r
+                    xij = coor[i-1][0] + dx * blen1
+                    yij = coor[i-1][1] + dy * blen1
+                    zij = coor[i-1][2] + dz * blen1
+                    restriction = False
+                    # all monomers outside of NP
+                    if (np.sqrt(xij**2 + yij**2 + zij**2) < R+0.5):
+                        restriction = True
+                    if i >= 2:
+                        distx = xij - coor[i-2][0]
+                        disty = yij - coor[i-2][1]
+                        distz = zij - coor[i-2][2]
+                        if (np.sqrt(distx*distx+ disty*disty + distz*distz) <= dmin): # the minimum distance between atom 1 and atom 3
+                            restriction = True
+                        elif ((xij<xlo) or (xij>xhi) or (yij<ylo) or (yij>yhi) or (zij<zlo) or (zij>zhi)):
+                            restriction = True
+                        #elif ((xij-5)**2 + (yij-5)**2 + (zij-5)**2) < 25 
+                        #   restriction = True
+            coor[i][0] = xij
+            coor[i][1] = yij
+            coor[i][2] = zij
+        coor_polymer2[ j, : ] = coor[:,:]
+
+    coor_polymer2 = coor_polymer2.reshape(f2*M2, 3)
+    coords = np.concatenate((coords, coor_polymer2))
+
+
+    massA = core_mass
+    massB = 1
+
+
+    atomtypes = 2
+    bondtypes = 2
+    bxlo = min(coords[:,0]) - 0.1
+    bxhi = max(coords[:,0]) + 0.1
+    bylo = min(coords[:,1]) - 0.1
+    byhi = max(coords[:,1]) + 0.1
+    bzlo = min(coords[:,2]) - 0.1
+    bzhi = max(coords[:,2]) + 0.1
+
+    # Create molecule tag
+
+    molecule = np.ones(num_atoms)
+
+    # Charge
+
+    charge = np.zeros(num_atoms)
+
+    # Atom number                
+
+    num = np.arange(1,num_atoms+1)
+
+    # Type
+
+    types = np.ones(num_atoms) + 1  # 1 for the core, 2 for the rest
+    types[0] = 1
+
+    coordinates = np.zeros((num_atoms,7))
+
+    coordinates[:, 0] = num
+    coordinates[:, 1] = molecule
+    coordinates[:, 2] = types
+    coordinates[:, 3] = charge
+    coordinates[:, 4:] = coords
+
+
+    ### Bond matrix
+
+    # num_bonds_arm = arm_length - 1
+    # num_atoms = num_arms * arm_length + 1
+    # bonds = num_bonds_arm * num_arms + num_arms
+
+    num_bonds = num_atoms -1
+    bond = np.zeros((num_bonds, 4))
+    bond[:,0] = np.arange(1,num_bonds+1)
+
+    for k in range(f1):
+        
+        bond[ (k*(M1-1)):(k+1)*(M1-1), 1] = 2
+        bond[ (k*(M1-1)):(k+1)*(M1-1), 2] = np.arange( k*M1+2, (k+1)*M1+1)
+        bond[ (k*(M1-1)):(k+1)*(M1-1), 3] = np.arange( k*M1+3, (k+1)*M1+2)
+
+
+    for k in range(f2):
+        
+        bond[f1*(M1-1) + k*(M2-1) : f1*(M1-1) + (k+1)*(M2-1), 1 ] = 2
+        bond[f1*(M1-1) + k*(M2-1) : f1*(M1-1) + (k+1)*(M2-1), 2 ] = np.arange( f1*M1+2 + k*M2, f1*M1+(k+1)*M2+1)
+        bond[f1*(M1-1) + k*(M2-1) : f1*(M1-1) + (k+1)*(M2-1), 3 ] = np.arange( f1*M1+3 + k*M2, f1*M1+(k+1)*M2+2)    
+
+
+    # Bonds between arms and core
+
+    for k in range(f1):
+        bond[ f1*(M1-1)+f2*(M2-1) + (k+1)-1, 1] = 1
+        bond[ f1*(M1-1)+f2*(M2-1) + (k+1)-1, 2] = k * M1 + 2
+        bond[ f1*(M1-1)+f2*(M2-1) + (k+1)-1, 3] = 1
+
+
+    for k in range(f2):
+        bond[ f1*(M1-1)+f2*(M2-1)+f1 + (k+1)-1, 1] = 1
+        bond[ f1*(M1-1)+f2*(M2-1)+f1 + (k+1)-1, 2] = (f1*M1) + k * M2 + 2
+        bond[ f1*(M1-1)+f2*(M2-1)+f1 + (k+1)-1, 3] = 1
+
+
+    # Create LAMMPS data file                
+
+    with open(filename, 'w') as f:
+        
+        f.write(f'LAMMPS data file for coarse grained star with (f1,M1)=({f1},{M1}) (f2,M2)=({f2},{M2})  \n\n')
+        f.write('%1.0f atoms\n' % num_atoms)
+        f.write('%1.0f atom types\n' % atomtypes)
+        f.write('%1.0f bonds\n' % num_bonds)            
+        f.write('%1.0f bond types\n' % bondtypes)
+        f.write('0 angles\n')
+        f.write('0 angle types\n')
+        f.write('0 dihedrals\n')            
+        f.write('0 dihedral types\n')
+        f.write('%1.6f %1.6f xlo xhi\n' % (bxlo, bxhi))
+        f.write('%1.6f %1.6f ylo yhi\n' % (bylo, byhi))        
+        f.write('%1.6f %1.6f zlo zhi\n\n' % (bzlo, bzhi))
+        
+        f.write('Masses\n\n')
+        f.write('%1.0f %1.6f\n' % (1, massA))
+        f.write('%1.0f %1.6f\n\n'% (2, massB))
+        f.write('Atoms\n\n')
+        np.savetxt(f, coordinates, fmt='%4.0f %6.0f %6.0f %6.2f %12.6f %12.6f %12.6f')
+        f.write('\n')
+        f.write('Bonds\n\n')
+        np.savetxt(f, bond, fmt='%4.0f %6.0f %6.0f %6.0f')
