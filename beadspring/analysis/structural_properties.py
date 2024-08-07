55611a7c91f207f2b05d66aa1eab36c68fd1214d
utku <19888110+utkugurel@users.noreply.github.com>
2024-08-07 10:59:41 +0200

    added contacts, rdf and bounding sphere functions

--- beadspring/analysis/structural_properties.py
+++ beadspring/analysis/structural_properties.py
@@ -1,19 +1,22 @@
 import freud
 import numpy as np
 from MDAnalysis.analysis.rdf import InterRDF
+from MDAnalysis.analysis import contacts
+from scipy.spatial import ConvexHull
 
 
-def compute_rdf(box, positions, r_max=6.0, bins=50):
+
+def compute_rdf(positions, box, r_max=6.0, bins=50):
     """
     Computes the radial distribution function (RDF)
     at a given frame
 
     Parameters
     ----------
-    box : freud.box.Box
-        freud box item ([Lx, Ly, Lz])
     positions : np.ndarray
         Positions of particles with the shape (N, 3)
+    box : freud.box.Box
+        freud box item ([Lx, Ly, Lz])
     r_max : float
         Maximum distance to compute the RDF
     Returns
@@ -37,7 +40,7 @@ def compute_rdf(box, positions, r_max=6.0, bins=50):
     return rdf.bin_centers, rdf.rdf, r_min, r_peak
 
 
-def compute_rdf_pair(ag1, ag2, r_max=6.0, nbins=75):
+def compute_rdf_pair_mda(ag1, ag2, r_max=6.0, nbins=75):
     """
     Computes the averaged radial distribution function (RDF)
     between two groups of atoms. From MDAnalysis documentation:
@@ -83,6 +86,58 @@ def compute_rdf_pair(ag1, ag2, r_max=6.0, nbins=75):
     return rdf_bin_centers, rdf_vals, r_min, r_peak
 
 
+def compute_rdf_pair_freud(type1_positions, type2_positions, box, r_max=6.0, nbins=50):
+    """
+    
+    Computes the radial distribution function (RDF) between two groups of particles
+    using the freud library (much faster than the previous implementation)
+
+    Parameters
+    ----------
+    type1_positions : np.ndarray
+        Positions of group A particles with the shape (N, 3)
+    type2_positions : np.ndarray
+        Positions of group B particles with the shape (M, 3)
+    box : freud.box.Box
+        freud box item ([Lx, Ly, Lz])
+    r_max : float, optional
+        Maximum distance to compute the RDF. The default is 6.0.
+    bins : int, optional
+        Number of bins for the histogram. The default is 50.
+
+    Returns
+    -------
+    rdf_bin_centres : np.ndarray
+        bins of radial distribution function
+    rdf_ab_vals : np.ndarray
+        radial distribution function
+
+    """ 
+    # Wrap the positions within the box
+    type1_wrapped = box.wrap(type1_positions)
+    type2_wrapped = box.wrap(type2_positions)
+
+    # Define the query objects
+    query_type1 = freud.locality.AABBQuery(box, type1_wrapped)
+
+    # Initialize the RDF calculator
+    rdf = freud.density.RDF(bins=nbins, r_max=r_max)
+    
+    # Compute the RDF
+    rdf.compute(system=query_type1, query_points=type2_wrapped, reset=False)
+
+    # Retrieve the RDF data
+    rdf_bin_centres = rdf.bin_centers
+    rdf_12_vals = rdf.rdf
+    
+    max_ind = np.where(rdf_12_vals == max(rdf_12_vals))[0][0]
+    index = np.where(rdf_12_vals == min(rdf_12_vals[max_ind:]))[0][0]
+    r_min = rdf_bin_centres[index]
+    r_peak = rdf_bin_centres[max_ind]
+
+    return rdf_bin_centres, rdf_12_vals, r_min, r_peak
+
+
 def compute_structure_factor_freud(positions, box, bins=100, kmax=8.0, kmin=0.1):
     """
     Computes the static structure factor for a given trajectory
@@ -115,3 +170,68 @@ def compute_structure_factor_freud(positions, box, bins=100, kmax=8.0, kmin=0.1)
     sf.compute((box, wrapped_positions))
 
     return sf.bin_centers, sf.S_k
+
+
+def contacts_within_cutoff(type1_positions, type2_positions, box, radius=2.5):
+    '''
+    Computes the number of contacts between two groups of particles
+    within a given cutoff distance using the freud library
+    
+    Parameters
+    ----------
+    type1_positions : np.ndarray
+        Positions of group A particles with the shape (N, 3)
+    type2_positions : np.ndarray
+        Positions of group B particles with the shape (M, 3)
+    box : freud.box.Box
+        freud box item ([Lx, Ly, Lz])
+    radius : float, optional
+        Cutoff distance for the contact calculation. The default is 2.5.
+
+    Returns
+    -------
+    n_contacts : int
+        Number of contacts between the two groups of particles
+    '''
+
+    type1_wrapped = box.wrap(type1_positions) 
+    type2_wrapped = box.wrap(type2_positions)
+
+    dist = contacts.distance_array(type1_wrapped, type2_wrapped)
+    # determine which distances <= radius
+    n_contacts = contacts.contact_matrix(dist, radius).sum()
+    return n_contacts
+
+
+def bounding_sphere(positions):
+    """
+    Computes the bounding sphere of a set of points in 3D space
+
+    Parameters
+    ----------
+    positions : np.ndarray
+        (N, 3) shaped array containing the positions of particles
+    Returns
+    -------
+    radius : float
+        Radius of the bounding sphere
+    centre : np.ndarray
+        (3,) shaped array containing the center of the bounding sphere
+    """
+    
+    # Convert the list of tuples to a NumPy array
+    positions = np.array(positions)
+    
+    # Compute the convex hull
+    hull = ConvexHull(positions)
+    
+    # Get the vertices of the convex hull
+    hull_points = positions[hull.vertices]
+    
+    # Calculate the centroid of the convex hull vertices
+    centre = np.mean(hull_points, axis=0)
+    
+    # Calculate the radius as the maximum distance from the center to any convex hull vertex
+    radius = np.max(np.linalg.norm(hull_points - centre, axis=1))
+    
+    return radius, centre
