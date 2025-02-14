7319570a4c51b5109648333095a6636a6b29521b
utku <19888110+utkugurel@users.noreply.github.com>
2025-02-14 13:42:02 +0100

    code formatted with black

--- beadspring/analysis/structural_properties.py
+++ beadspring/analysis/structural_properties.py
@@ -36,7 +36,7 @@ def compute_rdf(positions, box, r_max=6.0, bins=50):
     index = np.where(rdf.rdf == min(rdf.rdf[max_ind:]))[0][0]
     r_min = rdf.bin_centers[index]
     r_peak = rdf.bin_centers[max_ind]
-    
+
     return rdf.bin_centers, rdf.rdf, r_min, r_peak
 
 
@@ -87,7 +87,7 @@ def compute_rdf_pair_mda(ag1, ag2, r_max=6.0, nbins=75):
 
 def compute_rdf_pair_freud(type1_positions, type2_positions, box, r_max=6.0, nbins=50):
     """
-    
+
     Computes the radial distribution function (RDF) between two groups of particles
     using the freud library (much faster than the MDA implementation)
 
@@ -111,7 +111,7 @@ def compute_rdf_pair_freud(type1_positions, type2_positions, box, r_max=6.0, nbi
     rdf_ab_vals : np.ndarray
         radial distribution function
 
-    """ 
+    """
     # Wrap the positions within the box
     type1_wrapped = box.wrap(type1_positions)
     type2_wrapped = box.wrap(type2_positions)
@@ -121,14 +121,14 @@ def compute_rdf_pair_freud(type1_positions, type2_positions, box, r_max=6.0, nbi
 
     # Initialize the RDF calculator
     rdf = freud.density.RDF(bins=nbins, r_max=r_max)
-    
+
     # Compute the RDF
     rdf.compute(system=query_type1, query_points=type2_wrapped, reset=False)
 
     # Retrieve the RDF data
     rdf_bin_centres = rdf.bin_centers
     rdf_12_vals = rdf.rdf
-    
+
     max_ind = np.where(rdf_12_vals == max(rdf_12_vals))[0][0]
     index = np.where(rdf_12_vals == min(rdf_12_vals[max_ind:]))[0][0]
     r_min = rdf_bin_centres[index]
@@ -137,7 +137,16 @@ def compute_rdf_pair_freud(type1_positions, type2_positions, box, r_max=6.0, nbi
     return rdf_bin_centres, rdf_12_vals, r_min, r_peak
 
 
-def compute_structure_factor_fourier(positions_type1, positions_type2, box_size, k_min, k_max, num_bins, grid_size=None, desired_dx=None):
+def compute_structure_factor_fourier(
+    positions_type1,
+    positions_type2,
+    box_size,
+    k_min,
+    k_max,
+    num_bins,
+    grid_size=None,
+    desired_dx=None,
+):
     """
     Compute the static structure factor S(k) and partial structure factors using FFT methods.
     Performs binning and returns binned k-values and S(k) values suitable for plotting.
@@ -184,12 +193,16 @@ def compute_structure_factor_fourier(positions_type1, positions_type2, box_size,
     rho2 = np.zeros_like(rho1)
 
     # Map positions to grid indices for type 1 particles
-    grid_indices1 = np.mod(positions_type1 / box_size * grid_size, grid_size).astype(int)
+    grid_indices1 = np.mod(positions_type1 / box_size * grid_size, grid_size).astype(
+        int
+    )
     for idx in grid_indices1:
         rho1[idx[0], idx[1], idx[2]] += 1.0
 
     # Map positions to grid indices for type 2 particles
-    grid_indices2 = np.mod(positions_type2 / box_size * grid_size, grid_size).astype(int)
+    grid_indices2 = np.mod(positions_type2 / box_size * grid_size, grid_size).astype(
+        int
+    )
     for idx in grid_indices2:
         rho2[idx[0], idx[1], idx[2]] += 1.0
 
@@ -214,7 +227,7 @@ def compute_structure_factor_fourier(positions_type1, positions_type2, box_size,
     kz = fftshift(k_values)
 
     # Create a grid of k-vector magnitudes
-    kx_grid, ky_grid, kz_grid = np.meshgrid(kx, ky, kz, indexing='ij')
+    kx_grid, ky_grid, kz_grid = np.meshgrid(kx, ky, kz, indexing="ij")
     k_magnitude = np.sqrt(kx_grid**2 + ky_grid**2 + kz_grid**2)
 
     # Flatten the arrays
@@ -293,10 +306,10 @@ def compute_structure_factor_freud(positions, box, bins=100, kmax=8.0, kmin=0.1)
 
 
 def contacts_within_cutoff(type1_positions, type2_positions, box, radius=2.5):
-    '''
+    """
     Computes the number of contacts between two groups of particles
     within a given cutoff distance using the freud library
-    
+
     Parameters
     ----------
     type1_positions : np.ndarray
@@ -312,9 +325,9 @@ def contacts_within_cutoff(type1_positions, type2_positions, box, radius=2.5):
     -------
     n_contacts : int
         Number of contacts between the two groups of particles
-    '''
+    """
 
-    type1_wrapped = box.wrap(type1_positions) 
+    type1_wrapped = box.wrap(type1_positions)
     type2_wrapped = box.wrap(type2_positions)
 
     dist = contacts.distance_array(type1_wrapped, type2_wrapped)
@@ -338,20 +351,20 @@ def bounding_sphere(positions):
     centre : np.ndarray
         (3,) shaped array containing the center of the bounding sphere
     """
-    
+
     # Convert the list of tuples to a NumPy array
     positions = np.array(positions)
-    
+
     # Compute the convex hull
     hull = ConvexHull(positions)
-    
+
     # Get the vertices of the convex hull
     hull_points = positions[hull.vertices]
-    
+
     # Calculate the centroid of the convex hull vertices
     centre = np.mean(hull_points, axis=0)
-    
+
     # Calculate the radius as the maximum distance from the center to any convex hull vertex
     radius = np.max(np.linalg.norm(hull_points - centre, axis=1))
-    
+
     return radius, centre
