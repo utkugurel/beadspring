1823040ff07e9d5b03266f68a9e4e047284853bc
utku <19888110+utkugurel@users.noreply.github.com>
2025-05-06 13:56:00 +0200

    added mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,22 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
+autodoc_mock_imports = [
+    'dask',
+    'freud',
+    'ipython',
+    'matplotlib',
+    'mdanalysis',
+    'numpy',
+    'ovito',
+    'pandas',
+    'scikit-learn',
+    'scipy',
+    'signac',
+    'signac-flow',
+    'numba',
+]
+
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -36,4 +52,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['']
+html_static_path = []
